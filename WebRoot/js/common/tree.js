7eeda58a27c470e5d3f68b3d66b85d73f613dac8
hecj <275070023@qq.com>
2014-12-15 01:37:02 +0800

    init project

---
+++ WebRoot/js/common/tree.js
@@ -0,0 +1,13 @@
+(function(){
+	
+	jQuery(document).ready(function(){
+		//加载树
+		jQuery('#west').panel({  
+		    href:'jsp/common/tree.jsp',  
+		    onLoad:function(){
+				
+		    }  
+		}); 
+	});
+
+})();
